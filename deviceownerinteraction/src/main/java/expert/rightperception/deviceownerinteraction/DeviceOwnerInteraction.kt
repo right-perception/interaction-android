4ecb0e2bf6db1a45dfaddbb221accebd49e36a32
Sergey Evstyunin <sergei.evstyunin@rightperception.company>
2022-03-14 19:29:13 +0400

    [ORG-387] Implement device owner interactions library

---
+++ deviceownerinteraction/src/main/java/expert/rightperception/deviceownerinteraction/DeviceOwnerInteraction.kt
@@ -0,0 +1,52 @@
+package expert.rightperception.deviceownerinteraction
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+
+internal object DeviceOwnerInteraction {
+
+    const val SCHEME = "owner"
+}
+
+object Kiosk {
+
+    private const val HOST = "kiosk"
+
+    private const val KEY_LOCK_TASK_PACKAGE_NAME = "lockTaskPackageName"
+    private const val KEY_LOCK_TASK_ENABLED = "lockTaskPackageName"
+
+    @SuppressLint("QueryPermissionsNeeded")
+    fun Activity.startKiosk() {
+        val uri = Uri.Builder()
+            .scheme(DeviceOwnerInteraction.SCHEME)
+            .query(HOST)
+            .appendQueryParameter(KEY_LOCK_TASK_PACKAGE_NAME, packageName)
+            .appendQueryParameter(KEY_LOCK_TASK_ENABLED, true.toString())
+            .build()
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        val activities = packageManager.queryIntentActivities(intent, 0)
+        if (activities.size > 0) {
+            startActivity(intent)
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    fun Activity.stopKiosk() {
+        stopLockTask()
+        val uri = Uri.Builder()
+            .scheme(DeviceOwnerInteraction.SCHEME)
+            .query(HOST)
+            .appendQueryParameter(KEY_LOCK_TASK_PACKAGE_NAME, packageName)
+            .appendQueryParameter(KEY_LOCK_TASK_ENABLED, false.toString())
+            .build()
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        val activities = packageManager.queryIntentActivities(intent, 0)
+        if (activities.size > 0) {
+            startActivity(intent)
+        }
+    }
+}
